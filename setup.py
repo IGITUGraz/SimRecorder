d91a056975d0726a34c12415094188b113fe99ff
Anand <anandtrex@users.noreply.github.com>
2018-02-26 17:07:25 +0100

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""
+This file installs the SimManager package.
+"""
+import re
+
+from setuptools import setup, find_packages
+
+from simrecorder import __version__, __author__
+
+
+def get_requirements(filename):
+    """
+    Helper function to read the list of requirements from a file
+    """
+    dependency_links = []
+    with open(filename) as requirements_file:
+        requirements = requirements_file.read().strip('\n').splitlines()
+    for i, req in enumerate(requirements):
+        if ':' in req:
+            match_obj = re.match(r"git\+(?:https|ssh|http):.*#egg=(\w+)-(.*)", req)
+            assert match_obj, "Cannot make sence of url {}".format(req)
+            requirements[i] = "{req}=={ver}".format(req=match_obj.group(1), ver=match_obj.group(2))
+            dependency_links.append(req)
+    return requirements, dependency_links
+
+
+requirements, dependency_links = get_requirements('requirements.txt')
+
+setup(
+    name="SimRecorder",
+    version=__version__,
+    packages=find_packages(),
+    author=__author__,
+    author_email="anand@igi.tugraz.at",
+    description="The Simulation Recorder is a library for recording data for scientific simulations.",
+    provides=['simrecorder'],
+    install_requires=requirements,
+    dependency_links=dependency_links,
+)
--- simrecorder/__init__.py
+++ simrecorder/__init__.py
@@ -1,8 +1,8 @@
+__author__ = "Anand Subramoney"
+__version__ = "0.9.0"
+
 from .recorder import Recorder
 
 __all__ = ['Recorder']
 
 REDIS_PORT = 65535
-
-
-
