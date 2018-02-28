c64b348021c30b4926ae4e26860c12f5bc814354
Anand <anandtrex@users.noreply.github.com>
2018-02-28 18:19:27 +0100

    Make pickle default serializer, with option to switch to pyarrow

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import re
 from setuptools import setup
 
 __author__ = "Anand Subramoney"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 
 def get_requirements(filename):
--- simrecorder/datastores.py
+++ simrecorder/datastores.py
@@ -1,8 +1,9 @@
-from warnings import warn
 import pickle
+from enum import Enum
 
-from rejson import Client, Path
+import pyarrow
 from rediscontroller import start_redis, stop_redis, is_redis_running
+from rejson import Client
 
 REDIS_PORT = 65535
 
@@ -55,12 +56,16 @@ class InMemoryDataStore(DataStore):
         return self.data.get(key, [])
 
 
+Serialization = Enum('Serialization', ['PICKLE', 'PYARROW'])
+
+
 class RedisDataStore(DataStore):
     """
     A datastore that persists all data to redis with the provided parameters
     """
 
-    def __init__(self, server_host, data_directory):  # , custom_json_encoder_cls=None, custom_json_decoder_cls=None):
+    def __init__(self, server_host, data_directory,
+                 serialization=Serialization.PICKLE):  # , custom_json_encoder_cls=None, custom_json_decoder_cls=None):
         # Either both are None or both are not
         # assert not ((custom_json_encoder_cls is None) ^ (custom_json_decoder_cls is None))
         # self.custom_json_encoder_cls = custom_json_encoder_cls
@@ -69,9 +74,33 @@ class RedisDataStore(DataStore):
         self.data_directory = data_directory
         self.rj = None
 
+        if serialization == Serialization.PICKLE:
+            self._serialize = self._pickle_serialize
+            self._deserialize = self._pickle_deserialize
+        elif serialization == Serialization.PYARROW:
+            self._serialize = self._pyarrow_serialize
+            self._deserialize = self._pyarrow_deserialize
+
+    @staticmethod
+    def _pickle_serialize(obj):
+        return pickle.dumps(obj)
+
+    @staticmethod
+    def _pickle_deserialize(bstring):
+        return pickle.loads(bstring)
+
+    @staticmethod
+    def _pyarrow_serialize(obj):
+        return pyarrow.serialize(obj).to_buffer().to_pybytes()
+
+    @staticmethod
+    def _pyarrow_deserialize(bstring):
+        return pyarrow.deserialize(pyarrow.frombuffer(bstring))
+
     def connect(self):
         if is_redis_running():
-            raise RuntimeError("Redis is already running (maybe from a previous experiment?). Did you forget to change the port?")
+            raise RuntimeError(
+                "Redis is already running (maybe from a previous experiment?). Did you forget to change the port?")
 
         start_redis(data_directory=self.data_directory)
 
@@ -79,20 +108,22 @@ class RedisDataStore(DataStore):
         #     self.rj = Client(host=self.server_host, port=REDIS_PORT, encoder=self.custom_json_encoder_cls(),
         #                      decoder=self.custom_json_decoder_cls())  # , decode_responses=True)
         # else:
-        self.rj = Client(host=self.server_host, port=REDIS_PORT) # , decode_responses=True)
+        self.rj = Client(host=self.server_host, port=REDIS_PORT)  # , decode_responses=True)
         return self
 
     # def store(self, key, dict_obj):
     #     self.rj.jsonset(key, Path.rootPath(), dict_obj)
 
     def append(self, key, obj):
-        pickled_obj = pickle.dumps(obj)
-        self.rj.rpush(key, pickled_obj)
+        # pickled_obj = pickle.dumps(obj)
+        serialized_obj = self._serialize(obj)
+        self.rj.rpush(key, serialized_obj)
 
     def get(self, key):
         if self.rj.type(key) == b'list':
             results = self.rj.lrange(key, 0, -1)
-            return [pickle.loads(r) for r in results]
+            # return [pickle.loads(r) for r in results]
+            return [self._deserialize(r) for r in results]
         # return self.rj.get(key)
 
     def close(self):
