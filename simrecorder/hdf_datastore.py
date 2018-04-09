714f5d266a01fa04a8b2d25f7141a71126103de6
Anand <anandtrex@users.noreply.github.com>
2018-04-09 19:19:38 +0200

    hdf5 Optimizations!
    * Adding large hdf5 chunk cache (20GB)
    * Optimizing chunk size to be close to 1MB, but while retaining last dimension

--- simrecorder/hdf_datastore.py
+++ simrecorder/hdf_datastore.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import h5py
+import h5py_cache
 
 from simrecorder.datastore import DataStore
 
@@ -12,14 +13,15 @@ class HDF5DataStore(DataStore):
     """
 
     def __init__(self, data_file_pth):
-        assert h5py.version.hdf5_version_tuple >= (1,9,178), "SWMR requires HDF5 version >= 1.9.178 "
+        assert h5py.version.hdf5_version_tuple >= (1, 9, 178), "SWMR requires HDF5 version >= 1.9.178 "
         "If you have libhdf5 version >= 1.10 but get this error, try installing h5py from source"
         "See: http://docs.h5py.org/en/latest/build.html#source-installation"
 
         if not os.path.exists(data_file_pth):
             self.f = h5py.File(data_file_pth, 'w', libver='latest')
         else:
-            self.f = h5py.File(data_file_pth, 'r', libver='latest')
+            # self.f = h5py.File(data_file_pth, 'r', libver='latest')
+            self.f = h5py_cache.File(data_file_pth, 'r', chunk_cache_mem_size=20 * 1024 ** 3, libver='latest')  ## 20 GB
         self.i = 0
 
     def set(self, key, dict_obj):
@@ -35,14 +37,50 @@ class HDF5DataStore(DataStore):
             if d is not None:
                 assert isinstance(d, h5py.Dataset)
                 # https://stackoverflow.com/a/25656175
-                d.resize(d.shape[0]+1, axis=0)
+                d.resize(d.shape[0] + 1, axis=0)
                 d[-1:, ...] = obj
                 d.flush()
             else:
-                self.f.create_dataset(key, data=obj[None, ...], compression="lzf", maxshape=(None, *obj.shape))
+                self.f.create_dataset(key, data=obj[None, ...], compression="lzf", maxshape=(None, *obj.shape),
+                                      chunks=self._get_chunk_size(obj))
         else:
             self.f.create_dataset("{}/{}".format(key, self.i), data=obj)
             self.i += 1
+
+    def _get_chunk_size(self, obj):
+        """
+        Tries to optimize the chunk size (assuming 32-bit floats used) so that the chunk size is close to 1MB. The last
+        dimension size is maintained without change.
+        :param obj:
+        :return:
+        """
+        ## Makes sure chunk size is always 1MB!
+        desired_chunk_size_bytes = 1024 ** 2  # 8K # 1024**2  # 1MB
+        element_size_bytes = 4
+        # Assuming storage of 32-bit floats
+        if np.prod(obj.shape) * element_size_bytes <= desired_chunk_size_bytes:
+            return obj.shape
+        else:
+            ndim = len(obj.shape)
+            total_elements = desired_chunk_size_bytes / element_size_bytes
+            # elements_per_dim = np.power(total_elements, 1 / ndim)
+            # print('elements_per_dim', elements_per_dim)
+            cum_el = total_elements
+            # print('cum_el', cum_el)
+            last_el_s = obj.shape[-1]
+            cum_el /= last_el_s
+            shape = [1]
+            # print('cum_el', cum_el)
+            for i in range(ndim - 1):
+                # print(i, np.power(cum_el, 1 / (ndim - i - 1)))
+                s = np.minimum(obj.shape[i], np.power(cum_el, 1 / (ndim - i - 1)))
+                s = np.floor(s)
+                cum_el /= s
+                shape.append(int(s))
+            shape.append(int(last_el_s))
+            # print('cum_el ', cum_el)
+            # print("shape ", shape)
+            return tuple(shape)
 
     def get_all(self, key):
         d = self.f.get(key)
--- tests/time_test_hdf5.py
+++ tests/time_test_hdf5.py
@@ -22,34 +22,38 @@ class Timer:
 
 
 def main():
-    n_arrays = 10
-
-    arrays = np.random.rand(n_arrays, 10, 500, 20, 600)
-    # arrays = (np.random.rand(n_arrays, 10, 500, 20, 600) > 0.5).astype(np.float)
-    # arrays = np.random.rand(n_arrays, 10, 5, 2, 6)
-
+    read_only = False
     data_dir = os.path.expanduser('~/output/tmp/hdf5-test')
-    os.makedirs(data_dir, exist_ok=True)
     file_pth = os.path.join(data_dir, 'data.h5')
-    if os.path.exists(file_pth):
-        os.remove(file_pth)
     key = 'train/what'
+    bs = 10
+    sts = 500 * 20
+    ns = 200
 
-    ## WRITE
-    hdf5_datastore = HDF5DataStore(file_pth)
-    recorder = Recorder(hdf5_datastore)
+    if not read_only:
+        n_arrays = 10
 
-    for i in range(n_arrays):
-        array = arrays[i]
-        with Timer() as st:
-            recorder.record(key, array)
-        if i == 0:
-            hdf5_datastore.enable_swmr()
-        print("Storing took %.2fs" % st.difftime)
-    recorder.close()
-    ## END WRITE
+        arrays = np.random.rand(n_arrays, bs, sts, ns)
 
-    print("File size after write is %d MiB" % (int(os.path.getsize(file_pth)) / 1024 / 1024))
+        os.makedirs(data_dir, exist_ok=True)
+        if os.path.exists(file_pth):
+            os.remove(file_pth)
+
+        ## WRITE
+        hdf5_datastore = HDF5DataStore(file_pth)
+        recorder = Recorder(hdf5_datastore)
+
+        for i in range(n_arrays):
+            array = arrays[i]
+            with Timer() as st:
+                recorder.record(key, array)
+            if i == 0:
+                hdf5_datastore.enable_swmr()
+            print("Storing took %.2fs" % st.difftime)
+        recorder.close()
+        ## END WRITE
+
+        print("File size after write is %d MiB" % (int(os.path.getsize(file_pth)) / 1024 / 1024))
 
     ## READ
     hdf5_datastore = HDF5DataStore(file_pth)
@@ -58,9 +62,18 @@ def main():
     with Timer() as rt:
         l = recorder.get_all(key)
     print("Reading took %.2fs" % rt.difftime)
+
+    for i in range(10):
+        b = np.random.randint(bs)
+        st = np.random.randint(sts)
+        with Timer() as rrt:
+            ll = np.array(l[:5, b, st, :])
+        print("Into sub-array took %.2f" % rrt.difftime)
+
     with Timer() as rrt:
         l = np.array(l)
     print("Into array took %.2f" % rrt.difftime)
+
     print("Mean is", np.mean(l), l.shape)
 
     recorder.close()
